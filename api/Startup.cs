698d1567c03501249e549e2c7a45278cad29b80b
Fabrizio Montemayor <20170994@aloe.ulima.edu.pe>
2020-08-15 22:39:53 -0500

    Esqueleto terminado

--- api/Startup.cs
+++ api/Startup.cs
@@ -32,6 +32,12 @@ namespace api
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             }
             );
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -48,6 +54,8 @@ namespace api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
